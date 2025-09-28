ce50f0832ac50d370b5d17371cf24f09fcfa654f
just <whywynwmx@hotmail.com>
2025-09-28 16:26:27 +0800

    go mod tidy

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -7,13 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>抖音无水印视频下载</h1>
-        <p className="subtitle">基于 React & Go 构建</p>
       </header>
       <main>
         <Downloader />
       </main>
       <footer>
-        <p>在浏览器中打开 `F:\\new\\douyin\\web\\index.html` 文件</p>
       </footer>
     </div>
   );
